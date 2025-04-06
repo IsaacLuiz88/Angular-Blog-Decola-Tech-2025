b075b11d03f3e59bc9731069ee65de7540d8739b
Isaac <isaacluiz0808@gmail.com>
2025-04-06 13:22:59 -0300

    Ending project

---
+++ angular-blog/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+@Component({
+  selector: 'app-root',
+  standalone: true, 
+  imports: [RouterOutlet, HomeComponent, MenuBarComponent], 
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'] 
+})
+export class AppComponent {
+  title = 'angular-client';
+}
